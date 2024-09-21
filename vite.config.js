cc6e471ab70a01a5687d9629b7228fd11f8d3465
John Gachara <johngachara29@gmail.com>
2024-09-21 10:27:25 +0300

    add vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,30 @@ export default defineConfig({
     },
   },
   base: './',
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate', // or 'prompt'
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Phenom Ventures LTD',
+        short_name: ' Phenom',
+        description: 'Phenom Ventures LTD',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'wifi.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'wifi.svg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),],
 })
